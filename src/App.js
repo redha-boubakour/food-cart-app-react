13bb81be270d485b1c40d6c26a4804a869d2fd03
redha-boubakour <redha.boubakour@yahoo.fr>
2022-05-09 19:09:30 +0200

    preparing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import Cart from "./components/Cart/Cart";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <Cart />
+            <Header />
+            <main>
+                <Meals />
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
