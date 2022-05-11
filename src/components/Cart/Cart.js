7aab4819df2ff094060d8bccfedc7a6cb03c0802
redha-boubakour <redha.boubakour@yahoo.fr>
2022-05-11 17:50:04 +0200

    practice

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import Modal from "../UI/Modal";
 
 import classes from "./Cart.module.css";
 
-const Cart = () => {
+const Cart = (props) => {
     const cartItems = (
         <ul className={classes["cart-items"]}>
             {[{ id: "c1", name: "Sushi", amount: 2, price: 11.99 }].map(
@@ -15,14 +15,19 @@ const Cart = () => {
     );
 
     return (
-        <Modal>
+        <Modal onHideCart={props.onHideCart}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>$35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes["button--alt"]}>Close</button>
+                <button
+                    className={classes["button--alt"]}
+                    onClick={props.onHideCart}
+                >
+                    Close
+                </button>
                 <button className={classes.button}>Order</button>
             </div>
         </Modal>
